058d233f5619d16d09e15231946217b1fe60fbec
UVuruna <155318434+UVuruna@users.noreply.github.com>
2024-01-03 08:37:45 +0100

    Add files via upload

--- knight.py
+++ knight.py
@@ -1,11 +1,11 @@
 from chess import Chess
-import copy
 
 class Knight(Chess):
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # >>> Creating Object <<<
     def __init__(self, side, name=None) -> None:
         super().__init__(side)
+        self.type = 'Warrior'
         self.name = name
         self.x = (0 if self.side == 'w' else 7)
         self.y = (1 if self.name == 'L' else 6)  if name else 0
@@ -43,33 +43,5 @@ class Knight(Chess):
             self.x -=1 ; self.y -=2
             return self.x,self.y
         
-    def possibleMoves(self,TableDict=None):
-        if TableDict is None:
-            CurrentTableDict = Chess.currentTableDict()
-        else:
-            CurrentTableDict = TableDict
-
-        def polje(content):
-            return CurrentTableDict[content.x,content.y]
-        
-        possibleMoveAttack_List = []
-        possibleTake_List = []
-        possibleDefend_List = []
-        
-        for dir in self.direction:
-            possMove = copy.deepcopy(self)
-            while possMove.granica(): 
-                possMove.incrementation(dir)
-                if possMove.granica() and polje(possMove) == '':
-                    possibleMoveAttack_List.append(possMove.position())
-                    break
-                elif possMove.granica() and possMove.side !=polje(possMove).side:
-                    possibleTake_List.append(possMove.position())
-                    break
-                elif possMove.granica() and possMove.side ==polje(possMove).side:
-                    possibleDefend_List.append(possMove.position())
-                    break
-                else:
-                    break
-                         
-        return possibleMoveAttack_List, possibleTake_List, possibleDefend_List, possibleMoveAttack_List
+    def possibleMoves(self, tableDict):
+         return super().possibleMoves(tableDict)
