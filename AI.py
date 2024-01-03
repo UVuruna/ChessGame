f02bd86ff446f85ec3092c636fa7be5704548113
UVuruna <155318434+UVuruna@users.noreply.github.com>
2024-01-03 08:35:39 +0100

    AI.py

--- AI.py
+++ AI.py
@@ -9,71 +9,87 @@ from rook import Rook
 
 
 class AI():
-       
-    def attackedPieces(Turn,team=None,clas=None,defenders=None):
-        if team is None:
-            turnI = 'b'
-            turnII = 'w'
-        else:
-            turnI = 'w'
-            turnII = 'b'
 
-        def noKing(clas):
-            if clas:
+    def countExecutionMethod(method):
+        def wrapper(*args, **kwargs):
+            wrapper.counter += 1
+            print(f"{method}Ponavlja se {wrapper.counter}. put")
+            return method(*args, **kwargs)
+        wrapper.counter = 0
+        return wrapper
+
+    #@countExecutionMethod   
+    def attackedPieces(Turn,tableDict,ourTeam,king,defenders,ActDict):
+        def team(Turn,ourTeam):
+            if ourTeam is False:
+                if Turn==1:
+                    return piece.side == 'b'
+                else:
+                    return piece.side == 'w'
+            else:
+                if Turn==1:
+                    return piece.side == 'w'
+                else:
+                    return piece.side == 'b'
+        def noKing(king):
+            if king is False:
                 return not isinstance(piece,King)
             else:
                 return True     
-        def noDefenders(defenders):
+        def noDefender(defenders):
             if defenders:
-                return piece not in defenders
+                return piece not in list(defenders.keys())
             else:
                 return True
 
-        PossibleTakes = []
-        PossibleMoves = []
-
+        PossibleTakes = set()
+        PossibleMoves = set()
+        possibleActionsDefenders = set()
+        possibleActionsDict = {}
         for piece in Chess.pieces:
-            if noKing(clas) and noDefenders(defenders):
-                if Turn == 1 and piece.side == turnI:
-                    PossibleTakes += piece.possibleMoves()[1]
-                    PossibleTakes += piece.possibleMoves()[3]
-                    PossibleMoves +=piece.possibleMoves()[0]
-                elif Turn == -1 and piece.side == turnII:
-                    PossibleTakes += piece.possibleMoves()[1]
-                    PossibleTakes += piece.possibleMoves()[3]
-                    PossibleMoves +=piece.possibleMoves()[0]
-
-        return set(PossibleTakes), set(PossibleMoves)
-        
+            take = set() ; move = set()
+            if team(Turn,ourTeam) and noKing(king):
+                if noDefender(defenders):
+                    PossibleTakes.update(piece.possibleMoves(tableDict)[1])
+                    PossibleMoves.update(piece.possibleMoves(tableDict)[0])
+                    if ActDict:
+                        possibleActionsDict[piece]=set(set(piece.possibleMoves(tableDict)[1])|set(piece.possibleMoves(tableDict)[0]))
+                else:
+                    take.update(piece.possibleMoves(tableDict)[1])
+                    move.update(piece.possibleMoves(tableDict)[0])
+                    actionsAll = take|move
+                    possibleActionsDefenders.update(actionsAll&set(defenders[piece]))
+                    if ActDict:
+                        possibleActionsDict[piece]=set(actionsAll&set(defenders[piece]))
+        return PossibleTakes, PossibleMoves, possibleActionsDefenders, possibleActionsDict
+    
+    #@countExecutionMethod    
     def dangerZone(turn):
         Turn = turn
         CurrentTableDict = Chess.currentTableDict()
 
-        # Ovaj Method daje:
-            # selfKing =CLASS.KING
-            # selfKingActions =LISTA [xy,xy,xy]
+        #@AI.countExecutionMethod
         def selfKingActionsCalc():
+            global realKing
             selfKingActions = []
             for piece in Chess.pieces:
                 if Turn == 1 and isinstance(piece,King) and piece.side == 'w':
-                    selfKingActions += piece.possibleMoves()[0]
-                    selfKingActions += piece.possibleMoves()[1]
+                    selfKingActions += piece.possibleMoves(CurrentTableDict)[0]
+                    selfKingActions += piece.possibleMoves(CurrentTableDict)[1]
                     selfKing = copy.deepcopy(piece)
+                    realKing = piece
                 elif Turn == -1 and isinstance(piece,King) and piece.side == 'b':
-                    selfKingActions += piece.possibleMoves()[0]
-                    selfKingActions += piece.possibleMoves()[1]
+                    selfKingActions += piece.possibleMoves(CurrentTableDict)[0]
+                    selfKingActions += piece.possibleMoves(CurrentTableDict)[1]
                     selfKing = copy.deepcopy(piece)
+                    realKing = piece
             return selfKing, set(selfKingActions)
-        
-        # Ovaj Method moze da se koristi na dva nacina:
-            # 1. Da daje ko sve napada tu poziciju
-                    # Figure koje DIREKTNO napadaju Kralja == LIST [Class.CHESS]
-            # 2. Da daje koje pozicije su napadnuta ili branjene
-                    # Pozicije oko Kralja na koje ne sme da stane dobijaju se uz sledeci Method
+
+        #@AI.countExecutionMethod
         def enemyAttDefCalc(position,noKing):
             king = copy.deepcopy(selfKing)
             TableDict = CurrentTableDict.copy()
-            if noKing == 'noKing':
+            if noKing == True:
                 TableDict[king.position()] = ''
 
             enemyAttack = []
@@ -89,9 +105,8 @@ class AI():
                         enemyAttack.append(piece.position())
                         positionsAttacked.append(position)
             return enemyAttack,positionsAttacked
-        
-        # Ovaj Method daje:
-            # Ubacuje LISTE za prethodni METOD
+
+        #@AI.countExecutionMethod
         def enemyAttDefListCalc(positionList,noKing):
             enemyAttack = []
             for pos in positionList:
@@ -99,101 +114,68 @@ class AI():
                 if a:
                     enemyAttack +=a
             return set(enemyAttack)
- 
-        # Ovaj metod analizira dobijenu liniju i skracuje je do neophodnog dela (2 Defender ili blokiran Attacker)
-        # daje 3 podatka:
-                # IndirectAttackers  == Gde se nalaze napadaci koji INDIREKTNO napadaju
-                # Defender           == Ko su nase figure koje ne smemo pomeriti
-                # Blockable Lines    == Koje pravce napada mozemo izblokirati. Ovo cemo morati staviti u uslov samo ako napada jedna linija inace mora pomeranje kralja       
-        
-        def enemyRangersAttackCalc():
+     
+        #@AI.countExecutionMethod
+        def enemyArcherDanger():
             Defenders = {}
             BlockableLine = []
-            for i in range(8):
+        #@AI.countExecutionMethod
+            def iteratingLine(possMove,direction):
+                Line = []
+                BlockableLine = []
+                Defenders = {}
+                defenderCount = 0
+                while possMove.insideBorder():
+                    possMove.incrementation(direction)
+                    if possMove.insideBorder() and defenderCount <2:
+                        Line.append(possMove.position())
+                        if CurrentTableDict[possMove.position()] != '' and CurrentTableDict[possMove.position()].side != possMove.side:
+                            if CurrentTableDict[possMove.position()].type =="Archer" and direction in CurrentTableDict[possMove.position()].direction:
+                                if defenderCount ==0:
+                                    BlockableLine += Line
+                                else:
+                                    Defenders[defender] = Line
+                                return Defenders,BlockableLine
+                            else:
+                                return
+                        elif CurrentTableDict[possMove.position()] !='' and CurrentTableDict[possMove.position()].side == possMove.side:
+                            defenderCount +=1
+                            defender = CurrentTableDict[possMove.position()]
+                    else:
+                        return
+
+            for dir in Chess.direction:
                 possMove = copy.deepcopy(selfKing)
                 try:
-                    d,bl = iteratingLine(possMove,i)
+                    d,bl = iteratingLine(possMove,dir)
                     Defenders.update(d)
                     BlockableLine.extend(bl)
                 except TypeError: # za None situacije, koje su ceste.
                     continue
-            return Defenders,BlockableLine
-        
-        # Ovaj Method analizira jednu liniju, sta se desava na njoj, i potom ga sledeci metod provlaci kroz sve DIRECTIONS
-        def iteratingLine(possMove,dirIndex):
-            Line = []
-            BlockableLine = []
-            Defenders = {}
-            defenderCount = 0
-            while possMove.granica():
-                possMove.incrementation(possMove.direction[dirIndex])
-                if possMove.granica() and defenderCount <2:
-                    Line.append(possMove.position())
-                    if CurrentTableDict[possMove.position()] != '' and CurrentTableDict[possMove.position()].side != possMove.side:
-                        if isinstance(CurrentTableDict[possMove.position()],Queen):
-                            if defenderCount ==0:
-                                BlockableLine += Line
-                            else:
-                                Defenders[defender] = Line
-                            return Defenders,BlockableLine
-                        elif isinstance(CurrentTableDict[possMove.position()],Rook) and dirIndex < 4:
-                            if defenderCount ==0:
-                                BlockableLine += Line
-                            else:
-                                Defenders[defender] = Line
-                            return Defenders,BlockableLine
-                        elif isinstance(CurrentTableDict[possMove.position()],Bishop) and dirIndex > 3:
-                            if defenderCount ==0:
-                                BlockableLine += Line
-                            else:
-                                Defenders[defender] = Line
-                            return Defenders,BlockableLine
-                        else:
-                            return
-                    elif CurrentTableDict[possMove.position()] !='' and CurrentTableDict[possMove.position()].side == possMove.side:
-                        defenderCount +=1
-                        defender = CurrentTableDict[possMove.position()]
-                else:
-                    return
-
-
+            return dict(Defenders),set(BlockableLine)
         
         selfKing,selfKingActions = selfKingActionsCalc()
-        Defenders,BlockableLine = enemyRangersAttackCalc()
-        
+        Defenders,BlockableLine = enemyArcherDanger()
+        directAttacker: set = set(enemyAttDefCalc(selfKing.position(),noKing=False)[0]) 
+        defendedEnemies: set = enemyAttDefListCalc(selfKingActions,noKing=True) 
+        teamPossTake,teamPossMove,possibleActionsDefenders,possibleActionsDict  = AI.attackedPieces(turn,CurrentTableDict,ourTeam=True,king=False,defenders=Defenders,ActDict=True) 
+        directAttackerSolution = directAttacker&teamPossTake
+        blockableLineSolution = BlockableLine&teamPossMove
 
-        directAttackers: set = set(enemyAttDefCalc(selfKing.position(),None)[0]) 
-        defendedEnemies: set = enemyAttDefListCalc(selfKingActions,'noKing') # srediti ovo i prethodne, ne treba sve
- 
-
-        # Da li kralj moze da se izvuce, na koja polja moze da pobegne ili koga moze da pojede
         DangerKingSolve: set = selfKingActions - defendedEnemies
+        DangerTeamSolve: set = directAttackerSolution|blockableLineSolution if len(directAttacker)==1 else set()
+        TeamOptions: set = teamPossMove|teamPossTake|possibleActionsDefenders
+        possibleActionsDict[realKing]=DangerKingSolve
 
-        # Da li tim moze da spasi kralja 
-        teamPossTake,teamPossMove = AI.attackedPieces(turn,'ourTeam','noKing',None)
-        BlockingLineSolu = set(BlockableLine)&teamPossMove
-        RemovingDirAttackSolu = directAttackers&AI.attackedPieces(turn,'ourTeam','noKing',list(Defenders.keys()))[0]
-        DangerTeamSolve: set = BlockingLineSolu|RemovingDirAttackSolu if len(directAttackers)==1 else None
-
-        teamPossMove: set = teamPossMove if teamPossMove is not None else set()
-        teamPossTake: set = teamPossTake if teamPossTake is not None else set()
-        DangerKingSolve: set = DangerKingSolve if DangerKingSolve is not None else set()
-        DangerTeamSolve: set = DangerTeamSolve if DangerTeamSolve is not None else set()
-        TeamOptions: set = teamPossMove|teamPossTake
-
-        # Napraviti sa Rekurzijom da vrti dok ne napravi mat
-            # Pa onda neka rekurzija da pokaze na koje nacine moze da odradi mat sa datim figurama
-                # Na kraju da resava Mat zagonetke
-        
-        return selfKing,directAttackers,TeamOptions,Defenders,DangerKingSolve,DangerTeamSolve,CurrentTableDict
+        return selfKing,directAttacker,TeamOptions,Defenders,DangerKingSolve,DangerTeamSolve,CurrentTableDict,possibleActionsDict
 
 
-
+    #@countExecutionMethod
     def GameOverCheck(turn):
-        selfKing,directAttackers,TeamOptions,Defenders,DangerKingSolve,DangerTeamSolve,CurrentTableDict = AI.dangerZone(turn)
+        selfKing,directAttacker,TeamOptions,Defenders,DangerKingSolve,DangerTeamSolve,CurrentTableDict,possibleActionsDict = AI.dangerZone(turn)
         king = None
         Solution: set =  DangerKingSolve|DangerTeamSolve
-        StaleMate: set = Solution|directAttackers|TeamOptions
+        StaleMate: set = Solution|directAttacker|TeamOptions
 
         GameOver = None
         selfTeam = []
@@ -214,22 +196,22 @@ class AI():
             GameOver = 'StaleMate'
         if not StaleMate:
             GameOver = 'StaleMate'
-        if directAttackers and not Solution:
+        if directAttacker and not Solution:
             GameOver = 'CheckMate'
-        if directAttackers:
+        if directAttacker:
             king = selfKing.position()
 
-        return king,DangerKingSolve,directAttackers,DangerTeamSolve,Defenders,GameOver,CurrentTableDict
+        return king,DangerKingSolve,directAttacker,DangerTeamSolve,Defenders,GameOver,CurrentTableDict,possibleActionsDict
     
-
-    def CastlingCheck(turn,king):
+    #@countExecutionMethod
+    def CastlingCheck(turn,tableDict,king):
         castling = [{(0,4),(0,5),(0,6),(0,7)},{(0,4),(0,3),(0,2),(0,1),(0,0)},
                     {(7,4),(7,5),(7,6),(7,7)},{(7,4),(7,3),(7,2),(7,1),(7,0)}]
         CastlingList = [[(0,7),(0,4)],[(0,0),(0,4)],[(7,7),(7,4)],[(7,0),(7,4)]]
-        attackPos,attackPie = AI.attackedPieces(turn,None,None,None)
+        attackPos,attackPie = AI.attackedPieces(turn,tableDict,ourTeam=False,king=True,defenders=None,ActDict=False)[:2]
         attack = attackPos | attackPie
         kW,qW,kB,qB = 0,0,0,0 ; castlingOptions = [kW,qW,kB,qB]
-        castlingOptions[0],castlingOptions[1],castlingOptions[2],castlingOptions[3] = king.castlingCheck()
+        castlingOptions[0],castlingOptions[1],castlingOptions[2],castlingOptions[3] = king.castlingCheck(tableDict)
 
         if castling[0] & attack:
             castlingOptions[0] *=0
